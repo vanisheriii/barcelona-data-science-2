f04a7d7b28effcde6c5dec70ff0407449ba14a4c
Kevin Vincent John  Moscoso Sotelo <kevinms1@TTC.CAT>
2023-11-20 12:51:08 +0100

    Subiendo 1.3 casi completado y algunas recomendaciones

--- main.R
+++ main.R
@@ -9,5 +9,18 @@ library(XML)
 #Pregunta 1.1 agregando
 url <- "https://www.mediawiki.org/wiki/MediaWiki"
 web_page <-  httr::GET(url)
-xml_page <- htmlParse(web_page)
-xml_page
+xml_page <- htmlParse(web_page, asText=TRUE)
+
+
+#Pregunta 1.2 
+xml_title <- xpathApply(xml_page,"//title")
+xml_title
+
+#pregunta 1.3
+xml_a <- xpathApply(xml_page,"//a",xmlGetAttr, 'href')
+xml_a_href <- xpathApply(xml_page,"//a",xmlGetAttr, 'href')
+xml_a_desc <- xpathApply(xml_page,"//a//span")
+#xml_tile <- xml_tile ifelse(is.na(xml_tile),"")
+xml_a_desc
+##Usar el R vest
+#busar el r markdown
