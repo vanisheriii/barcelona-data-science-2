c8edd2d9cde8f7acce1cda3d607d1bd0c15acb52
Kevin Vincent John  Moscoso Sotelo <kevinms1@ttc.cat>
2023-11-20 12:10:43 +0100

    Instalando las librerias

--- main.R
+++ main.R
@@ -1,1 +1,14 @@
 #Inicio ejercicio 2
+#Instalar el paquete httr para descargar paginas web
+install.packages("httr")
+#Instalar paquete XML
+install.packages("XML")
+
+library(XML)
+
+
+url <- "https://www.mediawiki.org/wiki/MediaWiki"
+web_page <-  httr::GET(url)
+xml_page <- htmlParse(web_page)
+class(xml_page)
+class(web_page)
