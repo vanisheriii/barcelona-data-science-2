dd5b365176b4acdbe857c5132755b4ac6bb97518
Kevin Vincent John  Moscoso Sotelo <kevinms1@TTC.CAT>
2023-11-21 12:19:31 +0100

    Pregunta 1.5 al 80%

--- main.R
+++ main.R
@@ -9,37 +9,38 @@ install.packages("stringr")
 install.packages("writexl")
 
 
+#Usando las librerias
 library(XML)
 library(dplyr)
 library(writexl)
 
 
-#Pregunta 1.1 agregando
+#Pregunta 1.1 agregando Descargar la pagina web y parsear a formato xml
 url <- "https://www.mediawiki.org/wiki/MediaWiki"
 web_page <-  httr::GET(url)
 xml_page <-  htmlParse(web_page, asText=TRUE)
 print(web_page)
 
-#Pregunta 1.2 
+#Pregunta 1.2  buscar el tag title e imprimirlo
 xml_title <- xpathApply(xml_page,"//title")
 xml_title
 print(xml_title[1])
 
 
 #feature1.3
-print(xml_page)
-xml_page <- htmlParse(web_page, asText=TRUE)
+#print(xml_page)
+#xml_page <- htmlParse(web_page, asText=TRUE)
 
-xml_a <- xpathApply(xml_page,"//a",xmlGetAttr, 'href')
-xml_a_href <- xpathApply(xml_page,"//a",xmlGetAttr, 'href')
-xml_a_desc <- xpathApply(xml_page,"//a//span")
+#xml_a <- xpathApply(xml_page,"//a",xmlGetAttr, 'href')
+#xml_a_href <- xpathApply(xml_page,"//a",xmlGetAttr, 'href')
+#xml_a_desc <- xpathApply(xml_page,"//a//span")
 #xml_tile <- xml_tile ifelse(is.na(xml_tile),"")
-xml_a_desc
+#xml_a_desc
 ##Usar el R vest
 #busar el r markdown
 
 
-#Pregunta 1.3
+#Pregunta 1.3 buscar los valores a y el contenido de este mismo
 # Buscar todos los enlaces en la página
 enlaces <- xpathSApply(xml_page, "//a", xmlAttrs)
 
@@ -56,16 +57,15 @@ df$TEXTO = df$TEXTO <- ifelse(is.na(df$TEXTO),na,df$TEXTO)
 filter_duplicate  <- dplyr::filter(df,stringr::str_like(HREF,"/wiki/MediaWiki",ignore_case = TRUE))
  
 
-#1.4
+#1.4 ver la cantidad de URLS que han sido llamado mas de una vez y agruparlo con su texto
  
  df2 <- df %>% group_by(HREF) %>% summarise(cantidad_peticiones  = n())
  
- ##esto no va
- #inner_merged <- merge(df, df2, by = "HREF")
- #inner_merged
+ inner_merged <- merge(df, df2, by = "HREF")
+ inner_merged
  print(df2)
  
- #1.5
+#1.5 de la lista anterior se tiene que iterar y obtener los valores del URL
  
  url_test <- httr::GET(url)
  url_test$status_code
@@ -75,38 +75,44 @@ filter_duplicate  <- dplyr::filter(df,stringr::str_like(HREF,"/wiki/MediaWiki",i
  header
  
  
- for(i in 1:nrow(df2))
+ inner_merged <- cbind(inner_merged, responseCode=NA)
+ 
+ for(i in 1:nrow(inner_merged))
  {
-   row <- df2[i,]
-   #print(row$HREF)
-   url <- row$HREF
+   row <- inner_merged[i,]
+   url_local <- row$HREF
    
    ## aca tenemos que poner la condicional para saber comovamos tratar las url
    
-  # if(url == "https://www.mediawiki.org/wiki/Template:Main_page/bs")
-   #  print("found")
    #if(stringr::str_starts(url,"^//",negate = FALSE))
     # print(url)
+   
+   if(stringr::str_starts(url_local,"^//",negate = FALSE)){
+     # Reemplazar las dobles barras por una sola
+     url_local <- gsub("//", "/", url_local)
+     full_url = paste0(url,url_local)
+     print(full_url)
+   }
    
    
     #if(stringr::str_starts(url,"^/\\w+",negate = FALSE))
    #  print(url)
     
 
-   #if(stringr::str_starts(url,"https",negate = FALSE))
-   # print(url)
-   
-   if(stringr::str_starts(url,"http:",negate = FALSE))
-    print(url)
+   if(stringr::str_starts(url_local,"https",negate = FALSE)){
+     url_test <- httr::GET(url_local)
+     print(url_local)
+     print(url_test$status_code)
+     inner_merged[i,4] <- url_test$status_code
+     Sys.sleep(0.2)
+   }
    
    #if(stringr::str_starts(url,"#",negate = FALSE))
    #  print(url)
 
  }
 
- var1 <- df2$HREF
- 
+
 #Para exportar 
- 
 #write_xlsx(df, "df_pregunta1_3..xlsx")
 #write_xlsx(inner_merged, "df_pregunta1_3_inner..xlsx")
